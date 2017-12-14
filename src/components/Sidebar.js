fa0e649f402636db467703333ad3183172538d19
Nikolai Yakuschenko <you@example.com>
2017-12-14 19:42:20 +0300

    Implmented ...reducers

---
+++ src/components/Sidebar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+import { addDeck, showAddDeck, hideAddDeck } from '../actions';
+import { Link } from 'react-router';
+var createReactClass = require('create-react-class');
+
+const mapStateToProps = ({decks, addingDeck}) => ({
+  decks,
+  addingDeck
+}); 
+
+// Map dispatch function to 3 callbacks for Sidebar
+const mapDispatchToProps = dispatch => ({
+  addDeck: name => dispatch(addDeck(name)),
+  showAddDeck: () => dispatch(showAddDeck()),
+  hideAddDeck: () => dispatch(hideAddDeck())
+});
+
+const Sidebar = createReactClass({
+  componentDidUpdate() {
+    var el = ReactDOM.findDOMNode(this.refs.add);
+    if (el) el.focus();
+  },
+  render() {
+    let props = this.props;
+    {console.log('props = '+JSON.stringify(props))}
+    return (
+      <div className='sidebar'>
+        <h2>All Decks </h2>
+          <ul>
+          {props.decks.map((deck, i) => 
+            <li key={i}> 
+            <Link to={`/deck/${deck.id}`}> {deck.name} </Link>
+            </li>
+          )}
+          </ul>
+        {/* if props.addingDeck than...*/}
+          { props.addingDeck && <input ref='add' onKeyPress={this.createDeck} /> }
+      </div>
+    );
+  },
+  createDeck(evt) {
+    // if event not Enter key
+    if (evt.which !== 13) return;
+
+    // else Get the value of event (key pressed)
+    var name = ReactDOM.findDOMNode(this.refs.add).value;
+    this.props.addDeck(name);
+    this.props.hideAddDeck();
+  }
+});
+
+// Exporting container component
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
---
+++ src/index.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory  } from 'react-router';
+import { syncHistoryWithStore, routerReducer, ConnectedRouter  } from 'react-router-redux';
+import * as reducers from './reducers';
+// reducers.routing = routerReducer;
+
+import * as localStore from './localStore';
+
+import App from './components/App';
+import VisibleCards from './components/VisibleCards';
+import NewCardModal from './components/NewCardModal';
+import EditCardModal from './components/EditCardModal';
+import StudyModal from './components/StudyModal';
+
+// Main reducer
+// const store = createStore(combineReducers(reducers), localStore.get());
+const store = createStore(combineReducers({ routing: routerReducer, 
+  ...reducers
+    })); 
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+function run() {
+  let state = store.getState();
+
+  localStore.set(state, ['decks', 'cards']);
+
+  ReactDOM.render((
+    <Provider store={store}>
+      <Router history={history}>
+          <Route path='/' component={App} >
+            <Route path='/deck/:deckId' component={VisibleCards}>
+              <Route path='/deck/:deckId/new' component={NewCardModal} />
+              <Route path='/deck/:deckId/edit/:cardId' component={EditCardModal} />
+              <Route path='/deck/:deckId/study' component={StudyModal} />
+            </Route>
+          </Route>
+      </Router>
+  </Provider>
+  ), document.getElementById('root'));
+}
+
+run();
+store.subscribe(run);
